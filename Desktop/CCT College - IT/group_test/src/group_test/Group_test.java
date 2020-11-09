28e0b1606d7531f0db6a076782469f436c715874
Caio Machado de Oliveira <65948627+caiomoliveira@users.noreply.github.com>
2020-11-09 19:47:03 +0000

    Update Group_test.java

--- Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
+++ Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
@@ -15,7 +15,7 @@ public class Group_test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testando");
+        System.out.println("testado com sucesso!");
     }
     
 }
