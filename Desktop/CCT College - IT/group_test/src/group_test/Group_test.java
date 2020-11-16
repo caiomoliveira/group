291cd750e4fda4fce68bd46f30588e8f666a60b6
Wallace Esteves <wallace_esteves@hotmail.com>
2020-11-16 16:58:25 +0000

    Update Group_test.java

--- Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
+++ Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
@@ -10,32 +10,12 @@ package group_test;
  * @author 35383
  */
 public class Group_test {
-    static void Mayaramethod () {
-        /**
-        *author: Mayara
-        */
-        System.out.println("This is mayara's code");
-       
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("testado com sucesso!");
         System.out.println("testado com sucesso! Group R "); // Willian test
         System.out.println("Build Successful!");//Mayara
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 
-        Mayaramethod ();
-
-=======
-        WillianMethood();
->>>>>>> 3d43267f39e1a77e7c8389a12b574332858659dc
     }
-=======
 
-
->>>>>>> Stashed changes
-    }
-    
+     }
 }
