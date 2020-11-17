ece5320619b7ab1117f3ba9d21c0564a02ab3b09
Wallace Esteves <wallace_esteves@hotmail.com>
2020-11-17 13:44:10 +0000

    updated 17

--- Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
+++ Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
@@ -12,28 +12,23 @@ import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
  * @author 35383
  */
 public class Group_test {
-<<<<<<< HEAD
-    static void Mayaramethod () {
+    static void Mayaramethod() {
         /**
-        *author: Mayara
-        */
+         * author: Mayara
+         */
         System.out.println("This is mayara's code");
         return;
-       
-    /**
-     * @param args the command line arguments
-     * 
-     */
     }
 
-    static void WillianMethood(){
+    static void WillianMethood() {
         /**
-        * @willian
-        */
+         * @willian
+         */
         System.out.println("This is Willian Methood");
         System.out.println("This is my prog");
         return;
     }
+
     static void wallace() {
         /**
          * @Wallace
@@ -41,19 +36,12 @@ public class Group_test {
         System.out.println("wallace here");
         return;
     }
+
     public static void main(String[] args) {
-        System.out.println("testado com sucesso!");
-        System.out.println("testado com sucesso! Group R "); // Willian test
-        System.out.println("Build Successful!");//Mayara
-        Mayaramethod () ;
+
+        Mayaramethod();
         WillianMethood();
         wallace();
-        System.out.println("Renner is having problem with github ahahahahahahahah");
-        System.out.println("Coffe is good");
-        System.out.println("this work is baddd");
-        System.out.println("Caio was here!");
-        System.out.println("One more line");
-        System.out.println("hi, Mayara here");
-        System.out.println("Test Renner");
+
     }
-    }
+}
