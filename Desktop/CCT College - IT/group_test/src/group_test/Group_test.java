316ad554eab6f410f694a3512f6e0608931af8cb
Willian091 <will.yeah@hotmail.com>
2020-11-23 16:00:52 +0000

    update

--- Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
+++ Desktop/CCT College - IT/group_test/src/group_test/Group_test.java
@@ -58,15 +58,26 @@ public class Group_test {
             }
 
     static void WillianMethood() {
-        /**
-         * @willian
-         */
-        System.out.println("This is Willian Methood");
-        System.out.println("This is my prog");
-        return;
+        //Willian
+    public static void main(String[]args) { 
+    
+    try{
+    
+    Scanner input = new Scanner(System.in);
+    
+    double a;
+    double b;
+    int operation;
+    
+        System.out.println("Type first number: ");
+        a = input.nextDouble();
+        System.out.println("Type second number: ");
+        b = input.nextDouble();
+        System.out.println("Type a number for an operation: " + "\n" + "Sum = 1" + "\n" + "Sub = 2" + "\n" + "Div = 3" + "\n" + "Mul = 4" + "\n" + "Mod = 5" + "\n" + "Exit = 6");
+        operation = input.nextInt();
     }
 
-    static void wallace() {
+        static void wallace() {
         /**
          * @Wallace
          */
